8f76e650c77a519b5f45569889d820e24c49763f
Dennis <dennis_a12@hotmail.com>
2022-06-03 11:52:21 +0200

    commented out connection to mysql temporarily

--- index.php
+++ index.php
@@ -19,12 +19,13 @@
     error_reporting(E_ALL);
 
     // Create connection
+    /*
     $conn = new mysqli($servername, $username, $password);
 
     // Check connection
     if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
-    }
+    }*/
 
     // Not really using db after this
 
